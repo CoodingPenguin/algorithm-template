c69957b59162377af377851bb68507f83f5b7928
CoodingPenguin <unodostre@naver.com>
2020-12-02 15:03:27 +0900

    Add rotate-matrix-by-90

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# algorithm-template
-📝 내가 만드는 알고리즘 템플릿
+# 내가 만드는 알고리즘 템플릿
+
+대표적인 알고리즘 사이트와 책을 가지고 공부하면서 꼭 알아야할 필수 알고리즘들을 **Python**으로 정리합니다!
+
+아래와 같은 사이트와 책을 참고하였으며 출처는 주석의 `src` 부분으로 확인하실 수 있습니다.
+
+- [백준 단계별 문제](https://www.acmicpc.net/step)
+- [프로그래머스 코딩테스트 준비](https://programmers.co.kr/learn/challenges?tab=all_challenges)
+- [이것이 취업을 위한 코딩테스트다 with 파이썬](http://www.yes24.com/Product/Goods/91433923)
+
+## Linear Algebra
+
+- [행렬 90도 회전](./linear-algebra/rotate-matrix-by-90.py)
---
+++ linear-algebra/rotate-matrix-by-90.py
@@ -0,0 +1,21 @@
+# title: 시계방향으로 행렬을 90도 회전
+# src: 이것이 취업을 위한 코딩테스트다 p. 520
+
+def rotate_a_matrix_by_90_degree(matrix):
+    row = len(matrix)       # 행의 길이
+    col = len(matrix[0])    # 열의 길이 
+
+    result = [[0]*row for _ in range(col)]
+    for i in range(row):
+        for j in range(col):
+            result[j][row-i-1] = matrix[i][j]
+    
+    return result
+
+
+if __name__ == '__main__':
+    before = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    after = rotate_a_matrix_by_90_degree(before)
+
+    for i in range(len(before)):
+        print(after[i])
